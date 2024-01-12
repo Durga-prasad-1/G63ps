d7035e5e49aef07457032cddc82bafbc25b92d71
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-12 13:00:58 +0530

    added html code of logout permission

--- Frontend/src/components/navbar/navlink2.jsx
+++ Frontend/src/components/navbar/navlink2.jsx
@@ -6,8 +6,16 @@ import "./navbar.css"
 
 function Navbar2(){
     const [showMediaIcons, setShowMediaIcons] = useState(false);
-
+    function showPermission(){
+        let show = document.querySelector("#permission");
+        show.classList.remove("active1");
+    }
+    function notShowPermission(){
+        let show = document.querySelector("#permission");
+        show.classList.add("active1");
+    }
     return(
+        <div>
         <nav className="nav">
             <div className="nav__logo">Thyroid Detection</div>
                 <ul className={
@@ -16,14 +24,30 @@ function Navbar2(){
                 <li className="link"><NavLink className="bb" to="/" target="_parent">Home</NavLink></li>
                 <li className="link"><NavLink className="bb" to="/about">About Us</NavLink></li>
                 <li className="link"><NavLink className="bb" to ="/contact">Contact</NavLink></li>
-                <li className="link" ><button className="btn" >Logout</button></li>
+                <li className="link" ><button className="btn" onClick={showPermission} >Logout</button></li>
                 </ul>            
                 <div className="show" onClick={() => setShowMediaIcons(!showMediaIcons)}>
                     <Menu />               
-                </div>
-
-                
+                </div>                
         </nav>
+        <div className="permission active1" id="permission">
+            <div className="decisionMaking">
+                <div className="hanuman">
+                <h6>Are you sure you want to log out?</h6>
+                <button style={{height:"20px",cursor:"pointer",backgroundColor:"white",border:"0"}} onClick={notShowPermission}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" >
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                </svg>
+                </button>
+                {/* <button>aut</button> */}
+                </div>                
+                <div className="yesOrNo">
+                    <button className="butn">Yes</button>
+                    <button className="butn" onClick={notShowPermission}>No</button>
+                </div>
+            </div>
+            </div>                        
+        </div>
     );
 }
 
