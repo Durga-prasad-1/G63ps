3d72e2da59d8dcd0f7baa039f9e740dfc5508ac1
Jayanth-137 <147145667+Jayanth-137@users.noreply.github.com>
2024-01-14 18:45:39 +0530

    Update deployment_g63.py

--- Server/ML_model/deployment_g63.py
+++ Server/ML_model/deployment_g63.py
@@ -1,19 +1,22 @@
 import pickle
 import sys
 import json
+import pandas as pd
 
-path = "C:/Users/jayan/OneDrive/Documents/KMIT/PS/Thyroid Detection/WebApp/server/ML_model/model.pkl"
+path = "C:/Users/jayan/OneDrive/Documents/KMIT/PS/Thyroid Detection/WebApp/server/ML_model/model1.pkl"
 
 with open(path, 'rb') as model_file:
     model = pickle.load(model_file)
 
 def predict(data):
+    df = pd.DataFrame(data, columns=['age', 'sex', 'TSH', 'T3', 'TT4', 'on_thyroxine', 'query_on_thyroxine', 'on_antithyroid_medication', 'sick', 'pregnant', 'thyroid_surgery', 'I131_treatment', 'query_hypothyroid', 'query_hyperthyroid', 'lithium', 'goitre', 'tumor', 'hypopituitary', 'psych'])
     # Make predictions using the loaded model
-    result = model.predict(data)
+    result = model.predict(df)
     return result.tolist()
 
 if __name__ == "__main__":
     # Parse input data from command line arguments
     data = json.loads(sys.argv[1])
-    prediction = predict([list(data.values())])
+    input_data = [list(data.values())]
+    prediction = predict(input_data)
     print(json.dumps(prediction))
