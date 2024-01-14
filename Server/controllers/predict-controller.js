30be3969b2ba5e66abe66339c28f50f04d2abf09
Jayanth-137 <147145667+Jayanth-137@users.noreply.github.com>
2024-01-14 18:48:35 +0530

    Update predict-controller.js

--- Server/controllers/predict-controller.js
+++ Server/controllers/predict-controller.js
@@ -1,45 +1,44 @@
 const { model } = require("mongoose");
 const pred_model = require("../models/prediction-model");
-const {PythonShell} = require("python-shell");
+const { PythonShell } = require("python-shell");
 
-const predictForm = async (req, res)=>{
+const predictForm = async (req, res) => {
     try {
-        const response = req.body;
+        const dataToPredict = req.body;
+        delete dataToPredict.Name;
+
         const modelPrediction = async (data) => {
             const options = {
                 mode: "text",
-                scriptPath:"C:/Users/jayan/OneDrive/Documents/KMIT/PS/Thyroid Detection/WebApp/server/ML_model",
+                scriptPath: "./ML_model",
                 args: JSON.stringify(data),
-            };  
+            };
 
-            return new Promise((resolve, reject)=>{
-                PythonShell.run("deployment_g63.py",options).then((err, results)=>{
-                    if (err){
-                        reject(err);
-                    }else {
-                        resolve(JSON.parse(results[0]));
+            return new Promise((resolve, reject) => {
+                PythonShell.run("deployment_g63.py", options).then((results) => {
+                    try {
+                        const prediction = JSON.parse(results[0].replace(/'/g, '"'));
+                        resolve(prediction);
+                    } catch (error) {
+                        console.log("Error in parsing the data", error);
+                        reject(error);
                     }
+                }).catch((err) => {
+                    console.log("Error in executing the Python script: ", err);
+                    reject(err);
                 });
-                //const pyshell = new PythonShell("deployment_g63.py", options);
-
-                // pyshell.on("message", (message) => {
-                //     resolve(JSON.parse(message));
-                // });
-
-                // pyshell.on("error", (err) => {
-                //     reject(err);
-                // });
             });
         };
-        const dataToPredict = req.body;
-        delete dataToPredict.Name;
-        //const Data = Object.values(dataToPredict);
+
         const prediction = await modelPrediction(dataToPredict);
-        console.log("Prediction: ",prediction);
+        res.json({prediction });
 
     } catch (error) {
+        console.log("errorrorror");
         console.log(error);
+        res.status(500).json({ error: "Internal Server Error" });
+        return;
     }
-}
+};
 
-module.exports = {predictForm};
+module.exports = { predictForm };
