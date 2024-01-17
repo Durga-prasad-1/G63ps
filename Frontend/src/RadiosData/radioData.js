7810733799308576871059bd857393fd6def5928
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-17 15:02:13 +0530

    added keys

--- Frontend/src/RadiosData/radioData.js
+++ Frontend/src/RadiosData/radioData.js
@@ -2,52 +2,62 @@ const radio = [
     {
         name :"On-Thyroid",
         yes :"Yes",
-        no : "No"
+        no : "No",
+        key:1
     },
     {
         name:"On-Antithyroid-Medication",
         yes :"Yes1",
-        no : "No1"
+        no : "No1",
+        key:2
     },
     {
         name:"Sick",
         yes :"Yes2",
-        no : "No2"
+        no : "No2",
+        key:3
     },
     {
         name:"Pregnant",
         yes :"Yes3",
-        no : "No3"
+        no : "No3",
+        key:4
     },
     {
         name:"Thyroid-Surgery",
         yes :"Yes4",
-        no : "No4"
+        no : "No4",
+        key:5
     },
     {
         name:"I131-Treatment",
         yes :"Yes5",
-        no : "No5"
+        no : "No5",
+        key:6
     },
     {
         name:"Goitre",
         yes :"Yes6",
-        no : "No6"
+        no : "No6",
+        key:7
     },
     {
         name:"Tumor",
         yes :"Yes7",
-        no : "No7"
+        no : "No7",
+        key:8
     },
     {
         name:"Hypopituitary",
         yes :"Yes8",
-        no : "No8"
+        no : "No8",
+        key:9
     },
     {
         name:"Psych",
         yes :"Yes9",
-        no : "No9"
+        no : "No9",
+        key:10
     },
 ];
 
