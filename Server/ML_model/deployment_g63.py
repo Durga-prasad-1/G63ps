20a2d709597c5f96f1a929463391cd895b34622a
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-01-19 14:16:59 +0530

    Update deployment_g63.py

--- Server/ML_model/deployment_g63.py
+++ Server/ML_model/deployment_g63.py
@@ -10,7 +10,6 @@ with open(path, 'rb') as model_file:
 
 def predict(data):
     df = pd.DataFrame(data, columns=['age', 'sex', 'TSH', 'T3', 'TT4', 'on_thyroxine', 'query_on_thyroxine', 'on_antithyroid_medication', 'sick', 'pregnant', 'thyroid_surgery', 'I131_treatment', 'query_hypothyroid', 'query_hyperthyroid', 'lithium', 'goitre', 'tumor', 'hypopituitary', 'psych'])
-    # Make predictions using the loaded model
     result = model.predict(df)
     return result.tolist()
 
@@ -18,5 +17,11 @@ if __name__ == "__main__":
     # Parse input data from command line arguments
     data = json.loads(sys.argv[1])
     input_data = [list(data.values())]
+    input_data[0][1]=int(input_data[0][1])
+    for i in range(len(input_data[0])):
+        if input_data[0][i] == "0" or input_data[0][i] == "1":
+            input_data[0][i]=int(input_data[0][i])
+        else:
+            input_data[0][i]=float(input_data[0][i]) 
     prediction = predict(input_data)
     print(json.dumps(prediction))
