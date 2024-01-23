5e97019e16e6e27de65971fb2f5466bb0ae2ecaa
Rishikumar2406 <147145504+Rishikumar2406@users.noreply.github.com>
2024-01-23 16:08:10 +0530

    Update profile.jsx

--- Frontend/src/components/profile/profile.jsx
+++ Frontend/src/components/profile/profile.jsx
@@ -1,22 +1,23 @@
 import React from "react";
 import "./profile.css";
 import pic1 from "../images/login.png";
-import { Link } from "react-router-dom";
 
 function Profile(){
 
     return(
-        <div class="profile-container">
-        <h3>PROFILE</h3>
+        <div className="profile_body">
+        <div className="profile-container">
+        <h3 className="profile_heading">PROFILEPROFILE</h3>
         <div className="profile-picture">
             <img src={pic1.png} alt="Profile Picture" />
         </div>
-        <h2>Arjun</h2>
-        <p>Arjunreddy@gmail.com</p>
-        <p>9827827842</p>
-        <p>Male</p>
-        <button type="submit">Update Profile</button>
-    </div>
+        <h1 className="profile_name">Arjun</h1>
+        <p className="profile_email">Arjunreddy@gmail.com</p>
+        <p className="profile_phone">9827827842</p>
+        <p className="profile_gender">Male</p>
+        <button type="submit" className="profile_button">Update Profile</button>
+        </div>
+        </div>
     )
 
 }
