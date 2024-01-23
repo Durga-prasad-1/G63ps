349a3e8ae9c6947a3918d6de87d493b40ba7ab0c
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-23 15:29:27 +0530

    added auth-middleware.js

---
+++ Server/middlewares/auth-middleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
+
+const authMiddleware = async (req, res, next)=>{
+    const token = req.header("Authorization");
+    if(!token){
+        return res.status(401).json({msg:"Unautharized HTTP, Token not provided."});
+    }
+    const jwtToken = localStorage.getItem('token');
+    
+    try {
+        const isVerified = jwt.verify(jwtToken,process.env.JWT_KEY);
+        console.log(isVerified);
+        const userData = await User.findOne({_id:isVerified.userId});
+
+        req.token = jwtToken;
+        req.user = userData;
+        req.userId = userData._id;
+    } catch (error) {
+        console.log(error);
+        return res.status(400).json({msg: "Unautharized. Invalid token."})
+    }
+    next();
+}
+
+module.exports = authMiddleware;
