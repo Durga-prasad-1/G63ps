22967dc63b817c7ef9b6ea69a08d17b61dff8fe1
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-24 14:48:03 +0530

    token added and updated profile

--- Frontend/src/components/profile/profile.jsx
+++ Frontend/src/components/profile/profile.jsx
@@ -5,9 +5,8 @@ import pic1 from "../images/login.png";
 function Profile(){
 
     return(
-        <div className="profile_body">
-        <div className="profile-container">
-        <h3 className="profile_heading">PROFILEPROFILE</h3>
+        <div class="profile-container">
+        <h3 className="profile_heading">PROFILE</h3>
         <div className="profile-picture">
             <img src={pic1.png} alt="Profile Picture" />
         </div>
@@ -16,8 +15,7 @@ function Profile(){
         <p className="profile_phone">9827827842</p>
         <p className="profile_gender">Male</p>
         <button type="submit" className="profile_button">Update Profile</button>
-        </div>
-        </div>
+    </div>
     )
 
 }
