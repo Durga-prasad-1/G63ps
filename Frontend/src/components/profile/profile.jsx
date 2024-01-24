5895f9b6e89dda0db111827e90da5361dc44397c
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-24 16:33:25 +0530

    update profile pic

--- Frontend/src/components/profile/profile.jsx
+++ Frontend/src/components/profile/profile.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import "./profile.css";
-import pic1 from "../images/login.png";
+import Avatar from '@mui/material/Avatar';
+import { jwtDecode } from "jwt-decode";
 
 function Profile(){
-
+    let user;
+    if (localStorage.getItem('token')){
+    user = jwtDecode(localStorage.getItem('token'));
+    }
+    // console.log(token);
     return(
         <div className="rishi_body">
-        <div class="profile-container">
+        <div className="profile-container">
         <h3 className="profile_heading">PROFILE</h3>
         <div className="profile-picture">
-            <img src={pic1} alt="Profile Picture" />
+            <Avatar src={user.picture} alt="profile pic" sx={{ width: 150, height: 150 }} />
         </div>
-        <h1 className="profile_name">Arjun</h1>
-        <p className="profile_email">Arjunreddy@gmail.com</p>
+        <h1 className="profile_name">{user.name}</h1>
+        <p className="profile_email">{user.email}</p>
         <p className="profile_phone">9827827842</p>
         <p className="profile_gender">Male</p>
         <button type="submit" className="profile_button">Update Profile</button>
