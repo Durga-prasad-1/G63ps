7b363bb2031ff6b42ea2212a4912ac8654fb647e
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-01-26 09:33:17 +0530

    Delete Server/router/reset-router.js

--- Server/router/reset-router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middlewares/auth-middleware");
-const pwdcontroller = require("../controllers/password-controller");
-
-router.route("/resetPassword").post(authMiddleware,pwdcontroller.resetPwd);
-
-module.exports = router;
