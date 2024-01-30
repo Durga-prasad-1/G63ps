a75ed13d174cae801e1595dd25ba78d4c52bb86d
Durga-prasad-1 <durgagundeti@gmail.com>
2024-01-30 15:51:06 +0530

    changes done in RadioBox file for test form

--- Frontend/src/components/forms/RadioBox.jsx
+++ Frontend/src/components/forms/RadioBox.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 function RadioBox(props){
     let h = props.name;
+    if (h.indexOf('?')>-1){ // this code is for the "test" file data retrive to useState
+        h.replace('?','')
+    }
     let joinName = h.replaceAll(" ",""); // removing "-" from name to access for object of formData
     // console.log(joinName);
+    //-------------start of test form code-----------
+    if (joinName.indexOf('.')>-1){
+        let fullstop = joinName.indexOf('.');
+        joinName = "S" + joinName.slice(0,fullstop);
+    }
+
 
     function handle(event){
         props.func({
             ...props.set,
             [event.target.name]: event.target.value,
+            
         });
         // console.log(props.set)
     }
     let des = "",des2="";
-    if (props.name=="Gender"){
+    if (props.name==="Gender"){
         des = "Female";
         des2 = "Male";
     }
@@ -22,7 +32,7 @@ function RadioBox(props){
     }
 
     return(<div className="box">
-    <p className="sub_title">{props.name+":"}</p>
+    <p className="sub_title">{props.name}</p>
     <br/>
     <div className="radio-buttons-container">
     <input type="radio" name={joinName} className="radio-button__input" onChange={handle} value="1" id={props.yes} />
