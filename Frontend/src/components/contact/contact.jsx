444d6849f4af79b5357466aa4823c4358377bc67
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-02-01 00:03:27 +0530

    Update contact.jsx

--- Frontend/src/components/contact/contact.jsx
+++ Frontend/src/components/contact/contact.jsx
@@ -23,7 +23,7 @@ function Contact(){
         event.preventDefault();
         console.log(contactdata);
         try {
-            const response = await fetch("http://localhost:5000/contact",{
+            const response = await fetch("https://thyroaid.onrender.com/contact",{
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
