dc2bdb32401258699e6aafcaacc2f0da9ee5287a
Durga-prasad-1 <durgagundeti@gmail.com>
2024-02-01 22:52:38 +0530

    removed logout button

--- Frontend/src/components/navbar/navlink2.jsx
+++ Frontend/src/components/navbar/navlink2.jsx
@@ -50,7 +50,7 @@ function Navbar2(props){
                 <li className="link" onClick={scrollToDiv}><span className="bb">About Us</span></li>
                 <li className="link"><NavLink className="bb" to ="/contact">Contact</NavLink></li>            
                 <li className="googleImg" ><AccountMenu link={google.picture}  func={props.func}/></li>
-                <li className="link menushow"><NavLink className="bb" to ="/profile">Contact</NavLink></li>
+                <li className="link menushow"><NavLink className="bb" to ="/profile">Profile</NavLink></li>
                 <li className="link menushow"><NavLink className="bb" to ="/resetPassword">Reset password</NavLink></li>
                 <li className="link menushow"><NavLink className="bb" to ="#">History</NavLink></li>
                 <li className="link menushow bb" onClick={showPermission}><span className="bb">Logout</span></li>
