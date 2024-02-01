76baf93bab11959b680e83e464088a831f76843a
Durga-prasad-1 <durgagundeti@gmail.com>
2024-02-01 21:45:35 +0530

    updated menu for mobiles

--- Frontend/src/components/navbar/navlink2.jsx
+++ Frontend/src/components/navbar/navlink2.jsx
@@ -4,10 +4,6 @@ import Menu from "./menu";
 import AccountMenu from "./task";
 import "./navbar.css"
 import { jwtDecode } from "jwt-decode";
-import ListItemIcon from '@mui/material/ListItemIcon';
-import MenuItem from '@mui/material/MenuItem';
-import Logout from '@mui/icons-material/Logout';
-import Avatar from '@mui/material/Avatar';
 
 
 
@@ -41,19 +37,7 @@ function Navbar2(props){
     // console.log(ti);
 
     // this is for profile , logout, reset password
-    const handleClose = (event) => {
-        let text="";
-        console.log(event.target.innerText);
-        text = event.target.innerText;
-        if (text === "Profile" ){
-            navigate("/profile");
-        }
-        if (text === "Reset password" ){
-            navigate("/resetPassword");
-        }
-        // <Resetpass />
-        // <Link to="/login" className="none">Login</Link>
-    };
+    
     
     return(
         <div>
@@ -64,30 +48,12 @@ function Navbar2(props){
                 }>
                 <li className="link"><NavLink className="bb" to="/"  onClick={props.func(true)}>Home</NavLink></li>
                 <li className="link" onClick={scrollToDiv}><span className="bb">About Us</span></li>
-                <li className="link"><NavLink className="bb" to ="/contact">Contact</NavLink></li>
-                <li className="link" ><button className="btn" onClick={showPermission} >Logout</button></li>
+                <li className="link"><NavLink className="bb" to ="/contact">Contact</NavLink></li>            
                 <li className="googleImg" ><AccountMenu link={google.picture}  func={props.func}/></li>
-                <li className="menushow">
-                    <MenuItem onClick={handleClose}>
-                        <Avatar /> Profile
-                    </MenuItem></li>
-                <li className="menushow">
-                <MenuItem onClick={handleClose}>
-                    <Avatar /> Reset password
-                </MenuItem>
-                </li>
-                <li className="menushow">
-                    <MenuItem onClick={handleClose}>
-                    <Avatar /> History
-                </MenuItem></li>
-                <li className="menushow">
-                <MenuItem className='logout' onClick={showPermission}>
-                    <ListItemIcon>
-                        <Logout fontSize="small" />
-                    </ListItemIcon>
-                    Logout
-                </MenuItem>
-                </li>
+                <li className="link menushow"><NavLink className="bb" to ="/profile">Contact</NavLink></li>
+                <li className="link menushow"><NavLink className="bb" to ="/resetPassword">Reset password</NavLink></li>
+                <li className="link menushow"><NavLink className="bb" to ="#">History</NavLink></li>
+                <li className="link menushow bb" onClick={showPermission}><span className="bb">Logout</span></li>
                 </ul>            
                 <div className="show" onClick={() => setShowMediaIcons(!showMediaIcons)}>
                     <Menu />               
