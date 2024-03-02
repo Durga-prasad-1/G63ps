3b09abd4babcf0a5b220c7299ef37c36f985c78f
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-03-02 19:45:52 +0530

    Update password-controller.js

--- Server/controllers/password-controller.js
+++ Server/controllers/password-controller.js
@@ -6,16 +6,16 @@ const resetPwd = async (req, res)=>{
         const {Password, NewPassword, ConfirmPassword} = req.body;
         const userData = req.user;
         const isPwdValid = await userData.PasswordCheck(Password);
-        if(NewPassword==ConfirmPassword){
-            res.status(400).json({msg:"Both Passwords cannot be same"});
-        }
         if(!isPwdValid){
-            res.status(400).json({msg:"Invalid Password."});
+            res.status(400).json({msg:"Invalid Password"});
         }
-        if(newPwd!=confirmPwd){
-            res.status(400).json({msg:"Passwords don't match."});
+        if(NewPassword!=ConfirmPassword){
+            res.status(400).json({msg:"Passwords don't match"});
         }
-        userData.password=newPwd;
+        if(Password==ConfirmPassword){
+            res.status(400).json({msg:"Passwords cannot be same"});
+        }
+        userData.password=newPassword;
         await userData.save();
         res.status(200).json({msg:"Password changed successfully."});
     } catch (error) {
