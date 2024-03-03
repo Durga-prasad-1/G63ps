20e1267ac46e9d7f37cf77e29869e84b87ed8ce1
Jayanth Karnati <147145667+Jayanth-137@users.noreply.github.com>
2024-03-03 12:31:56 +0530

    Update history.jsx

--- Frontend/src/components/History/history.jsx
+++ Frontend/src/components/History/history.jsx
@@ -8,7 +8,7 @@ export default function History() {
     React.useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch("https://thyro-aid.vercel.app/auth/history", {
+                const response = await fetch("https://thyro-aid.onrender.com/auth/history", {
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json",
