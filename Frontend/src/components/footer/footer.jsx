793563808f0007fe02666297959cdaf483a54847
Rishikumar2406 <rishikumarkomuravelli@gmail.com>
2024-03-05 00:21:55 +0530

    new file:   Frontend/src/components/footer/footer.css 	new file:   Frontend/src/components/footer/footer.jsx 	modified:   Frontend/src/components/home/Home.jsx

---
+++ Frontend/src/components/footer/footer.jsx
@@ -0,0 +1,46 @@
+// Footer.js
+import React from "react";
+import "./footer.css";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer>
+      <div className="footer_container">        
+      <div>
+        <h1>Thyro-Aid:</h1>
+        <br />
+        <p>Find easy way to check thyroid</p>
+      </div>
+
+        <div className="main2">
+          <p className="matter1">Follow Us</p>
+          <div className="icons1">
+            <FacebookIcon />
+            <InstagramIcon />
+             <YouTubeIcon />
+            <LinkedInIcon />
+          </div>
+        </div>
+        <div className="phone">    
+          <p className="matter1">Call Us</p>
+          <a>123-456-7899</a>      
+        </div>         
+      </div>
+      <hr />
+    
+      <div className="last">
+      <p>&copy; {currentYear} Thyroid Detection. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
+
+
+export default Footer;
+
